af0324f084c1f6acac31153f5bb52f315b716802
DavidPsyko <davidjohnsonny@gmail.com>
2022-04-21 22:41:44 -0700

    derp

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-const url = new URL('https://api.hatchways.io/assessment/blog/posts')
-
-const params = new URLSearchParams(url.searchParams);
-params.append('tag', 'tech');
-console.log(params)
+const success = {
+  "success": true
+}
 
 
-console.log( url)
+console.log(module)
 
-fetch(url, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' }
-    })
-  .then(data => data.text())
-  .then((text) => {
-    console.log('request succeeded with JSON response', text)
-  }).catch(function (error) {
-    console.log('request failed', error)
-  });
+
+
+
+// const response = 
+
+// fetch('https://api.hatchways.io/assessment/blog/posts', {
+//   method: 'GET',
+// })
+//   .then(data => data.text(), console.log(data))
+//   .then((text) => {
+//     console.log('request succeeded with JSON response', text)
+//   }).catch(function (error) {
+//     console.log('request failed', error)
+//   });
 
 
 app.get("/api/ping", (req, res) => {
-    // res.json
-    res.status(200).send("hi")
+    console.log(req.body)
+    res.status(200).send("Response status code: 200")
 })
 
 
-
 app.listen(3000)
+
---
+++ index.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+// const app = express();
+// const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+
+// const url = new URL('https://api.hatchways.io/assessment/blog/posts')
+
+// const params = new URLSearchParams(url.searchParams);
+// params.append('tag', 'tech');
+// console.log(params)
+
+
+// console.log( url)
+
+// fetch('https://localhost:3000/api/ping', {
+//     method: 'GET',
+//     headers: { 'Content-Type': 'application/json' }
+//     })
+//   .then(data => data.text())
+//   .then((text) => {
+//     console.log('request succeeded with JSON response', text)
+//   }).catch(function (error) {
+//     console.log('request failed', error)
+//   });
