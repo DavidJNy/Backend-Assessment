c77d8506cfbb5326246e04f3fec4bbd5c5ef1e91
DavidPsyko <davidjohnsonny@gmail.com>
2022-04-26 01:08:05 -0700

    figure out that promise

--- app.js
+++ app.js
@@ -7,45 +7,48 @@ const needTag = {error : "Tags parameter is required"}
 const needSort = {error: "sortBy parameter is invalid"}
 const needDirection = {error: "direction parameter is invalid"}
 
-const mainURL = new URL ('https://api.hatchways.io/assessment/blog/posts');
-
-let searchResult = []
-
-function getData (url) { fetch(url, {
-  method: 'GET',
-  })
-  .then(data =>data.json())
-  .then((text) => {
-    searchResult = text
-  }).catch(function (error) {
-    console.log('request failed', error)
-  });
+let mainURL = new URL ('https://api.hatchways.io/assessment/blog/posts')
+  
+  const verify = function (req, res, next) {
+    const {tag, sortBy, direction } = req.query
+    if (tag === undefined) {
+      next();
+    }
+    else {
+      console.log(tag)
+        const finalData = getData(mainURL)
+        // res.send(finalData)
+        // mainURL.searchParams.append("tag", tag)
+        // console.log(mainURL)
+        // searchResult = getData(mainURL)
+        // res.send(searchResult)
+        next()
+    } 
 }
-
-const logger = (req, res, next) => {
-  getData(mainURL.href)
-  next()
-}
-
-
-app.get("/", (req, res) => {
-  console.log(req.body)
-  res.status(200).send("Response status code: 200")
-  res.end
-})
 
 app.get("/api/ping", (req, res) => {
   res.status(200).send(success)
   res.end
 })
 
-app.get("/api/posts", logger, (req, res) => {
-  const {tag, sortBy, direction } = req.query
-  mainURL.searchParams.append("tag", tag)
-  console.log(mainURL)
-  res.send(searchResult)
-  res.end()
-})
+app.get("/api/posts", verify , (req, res) => {
+  async function getData (url) { await fetch(url)
+    .then(data => {
+      data.json();
+    })
+    .then(poo => {
+      return poo
+    })
+    .catch((err) => console.log(err));
+  }
+  
+    const searchResult = getData(mainURL)
+    console.log(searchResult)
+    res.send(searchResult)
+    res.status(200)
+    res.end()
+  }
+)
 
 
 app.listen(3000)
