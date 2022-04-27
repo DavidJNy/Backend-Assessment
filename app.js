bc9828c2687acda93cdae33550580c81013c7144
DavidPsyko <davidjohnsonny@gmail.com>
2022-04-26 23:00:44 -0700

    adding comments

--- app.js
+++ app.js
@@ -8,12 +8,23 @@ const needDirection = {error: "direction parameter is invalid"}
 
 let mainURL = new URL ('https://api.hatchways.io/assessment/blog/posts')
 
+// Here is what you will need to do to complete this task:
+// ● For every tag specified in the tags parameter, fetch the posts with that tag using
+// the Hatchways API(make a separate API request for every tag specified)
+// ● Combine all the results from the API requests above and remove all the repeated
+// posts(try to be efficient when doing this)
+// ● You will get a better score on our assessment if you can make concurrent
+// requests to the API(making the requests in parallel)(we understand that this job
+// is easier in some languages vs.others)
+
+
 const verify = async function  (req, res, next) {
-    const {tag, sortBy, direction } = req.query
-    if (tag === undefined) {
+    const {tags, sortBy, direction } = req.query
+    if (tags === undefined) {
       next();
     }
     else {
+      console.log(tags)
       next();
     } 
 }
@@ -30,6 +41,8 @@ app.get("/api/posts", verify , async (req, res) => {
     .catch((err) => {
       console.log(err.message);
     })
+
+    // sort and direction here after getting all the API request
   res.send(getJson)
   res.status(200);
   res.end();
